4bf903d31119e282a0eb1412f9e6462a6ebdc9bc
Monaliza Pereira <100326367+Monalizaps@users.noreply.github.com>
2022-10-17 19:18:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function darkTheme() {
-  document.body.classList.add("dark-theme")
+  document.getElementById("logo").src="assets/VARIAÇÕES DO LOGOTIPO DOURADO DEGRADE-09.png";
+  document.body.classList.add("dark-theme");
 }
 
 function lightTheme() {
-  document.body.classList.remove("dark-theme")
+  document.getElementById("logo").src="assets/gabriela bastos - variacoes do logotipo-09.png";
+  document.body.classList.remove("dark-theme");
 }
